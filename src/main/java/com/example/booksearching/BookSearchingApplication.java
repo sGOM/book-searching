c1caffd29a1db228e579863a0c1d32f0146827e9
sGOM <pooh6195@naver.com>
2023-07-24 17:02:09 +0900

    #1 - 자동 생성 클래스
    main, test의 자동 생성 클래스

---
+++ src/main/java/com/example/booksearching/BookSearchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksearching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSearchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSearchingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/booksearching/BookSearchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksearching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSearchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
