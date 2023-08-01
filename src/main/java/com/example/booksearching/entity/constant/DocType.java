16e81e79a48ff1997c21532579a64ce3acdab87d
sGOM <pooh6195@naver.com>
2023-08-01 18:15:06 +0900

    #2 - `DocType`의 기능 추가정의
    `String` -> `DocType`의 기능 정의

--- src/main/java/com/example/booksearching/entity/constant/DocType.java
+++ src/main/java/com/example/booksearching/entity/constant/DocType.java
@@ -10,4 +10,13 @@ public enum DocType {
     private final String type;
 
     DocType(String type) { this.type = type; }
+
+    public static DocType fromString(String typeString) {
+        for (DocType docType : DocType.values()) {
+            if (docType.getType().equals(typeString)) {
+                return docType;
+            }
+        }
+        throw new IllegalArgumentException("해당하는 열거형 상수를 찾을 수 없습니다: " + typeString);
+    }
 }
