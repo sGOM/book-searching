afedc75f8f7d9894b27308c3b7bcd4c1e7bbbe88
Heo SeokJin <pooh6195@naver.com>
2024-01-04 01:29:33 +0900

    #13 - 테스트 코드 네이밍 변경
    5c407348 commit에서 변경한 네이밍과 변경된 사항에 맞춰 테스트 코드 변경

--- src/test/java/com/example/booksearching/StubUtils.java
+++ src/test/java/com/example/booksearching/StubUtils.java
@@ -1,21 +1,19 @@
 package com.example.booksearching;
 
-import com.example.booksearching.entity.DocInfo;
-import com.example.booksearching.entity.constant.DocType;
+import com.example.booksearching.spring.entity.Book;
 
 import java.time.Year;
 
 public class StubUtils {
-    public static DocInfo createDocInfo() {
-        return DocInfo.of(
+    public static Book createDocInfo() {
+        return Book.of(
                 "PCY_20171122038330711",
-                DocType.BOOK,
                 "아임상 및 경증 우울증 자기관리법 효용성 연구",
                 "보건의료",
                 "500",
                 "이완요법은 이완을 통해 육체적, 정신적 긴장을 줄이는 것이다. 예로 명상과 MBSR과 같은 임상 연구가 진행 중이다. 이러한 프로그램은 경제성을 지니며 요가도 몸과 마음의 조절로 삶의 질을 향상시킨다. 무작위 대조연구에서도 우울증 해소에 요가의 효과성을 입증했지만 우울증상의 차이 등을 고려해 추가 연구가 필요하다.",
-                "오강섭",
-                "한국보건의료연구원",
+//                "오강섭",
+//                "한국보건의료연구원",
                 Year.of(2010)
         );
     }
---
+++ src/test/java/com/example/booksearching/spring/service/BookServiceTest.java
@@ -0,0 +1,39 @@
+package com.example.booksearching.spring.service;
+
+import com.example.booksearching.StubUtils;
+import com.example.booksearching.spring.entity.Book;
+import com.example.booksearching.spring.repository.BookRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@DisplayName("DocInfo 로직")
+@ExtendWith(MockitoExtension.class)
+class BookServiceTest {
+    @InjectMocks
+    private BookService sut;
+    @Mock
+    private BookRepository bookRepository;
+
+    @DisplayName("DocInfo를 건네면, 저장하고 저장한 DocInfo를 반환한다")
+    @Test
+    void givenDocInfo_whenSaving_thenSaveAndReturnDocInfo() {
+        // Given
+        Book book = StubUtils.createDocInfo();
+        given(bookRepository.save(book)).willReturn(book);
+
+        // When
+        Book res = sut.saveDocInfo(book);
+
+        // Then
+        assertEquals(res, book);
+        then(bookRepository).should().save(book);
+    }
+}
