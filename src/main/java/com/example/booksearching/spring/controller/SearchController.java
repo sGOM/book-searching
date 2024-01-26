b9e38d93ad0f2793b713505e7280fb4667c0de6a
Heo SeokJin <pooh6195@naver.com>
2024-01-26 15:07:56 +0900

    #41 - `Controller` 네이밍 변경 + 라우팅
    - 53136497 에서 했던 것과 같이 `Controller` 네이밍
    - `index.html`을 홈페이지로 설정
    - `자동 완성 후보 검색` 요청 분리
    - `/search` 요청의 경우 `search.html`을 페이지를 반환

---
+++ src/main/java/com/example/booksearching/spring/controller/SearchController.java
@@ -0,0 +1,41 @@
+package com.example.booksearching.spring.controller;
+
+import com.example.booksearching.spring.dto.BookInfoResponse;
+import com.example.booksearching.spring.service.SearchService;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/search")
+@Controller
+public class SearchController {
+
+    private final SearchService searchService;
+    
+    @GetMapping
+    public String getSearch(
+            String keyword,
+            @Positive @RequestParam(defaultValue = "1", required = false) int page,
+            @Positive @RequestParam(defaultValue = "24", required = false) int size,
+            Model map
+    ) {
+        map.addAttribute("data", searchService.searchBooks(keyword, page, size));
+
+        return "search";
+    }
+
+    @GetMapping("/auto-complete")
+    @ResponseBody
+    public List<BookInfoResponse> getAutocompleteSuggestions(String keyword) {
+        return searchService.searchAutocompleteSuggestions(keyword);
+    }
+
+}
